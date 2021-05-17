90ee00250369dddef7bc9879195ffe08f0e7e416
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-05-17 23:52:52 +0530

    singleton pattern

--- design_patterns.js
+++ design_patterns.js
@@ -42,5 +42,35 @@ employees.push(empFactory.create("James", SoftwareEngineer.DEV));
 employees.push(empFactory.create("Ray", SoftwareEngineer.QA));
 
 employees.forEach((emp)=>{
-    say.call(emp);
+    // say.call(emp);
 })
+
+// -----------------X--------------------------X--------------------------X--------------------
+// 2. Singleton Pattern
+
+const Singleton = (function () {
+    function ProcessManager() {
+        this.numOfProcess = 0;
+        console.log('>> obj created');
+    }
+
+    let pManager = null;
+
+    function createProcessManager() {
+        pManager = new ProcessManager();
+        return pManager;
+    }
+
+    return {
+        getProcessManager:function () {
+            if(pManager===null)
+                pManager = createProcessManager();
+            return pManager;
+        }
+    };
+})();
+
+const pm1 = Singleton.getProcessManager();
+const pm2 = Singleton.getProcessManager();
+
+console.log('>> pm1 === pm2',pm1 === pm2);
