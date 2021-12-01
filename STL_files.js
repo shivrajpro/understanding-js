b266750eb7d6d13cea9875c2d687625a1afffd24
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-12-01 14:17:07 +0530

    forming concurrent api requests

---
+++ STL_files.js
@@ -0,0 +1,90 @@
+const uploadedFiles = {
+    "contact_id": "61a7074f46e0fb000714160b",
+    "folder_id": "151280270221",
+    "parent_id": "151280270221",
+    "box_metadata": {
+        "890835679273": {
+            "name": "stl file 1.stl",
+            "isFolder": false,
+            "fileSize": 316084,
+            "fileType": "stl",
+            "thumbnailLink": "https://patientfilethumbnail.s3.ap-southeast-2.amazonaws.com/stil%20file%2011638340256742.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211201T063057Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3598&X-Amz-Credential=AKIA4L27K4CWP2A2Y3FF%2F20211201%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Signature=36ce004cb774ef143ff9a2b2019eb4352c81a210d5c6e5765b3a1dd5fc883864",
+            "expirationTime": 1638343856742,
+            "thumbnailName": "stil file 11638340256742.png",
+            "downloadUrl": "https://dl.boxcloud.com/d/1/b1!ZPKYT7e5-XZxwVNIlM_r79TlyrOfsWuo77QThRWDTuF18eq6EtGdWxfI-LLrsXXCM7Jj_3EfGJrcIo42pL72k-3aRNmEqFe3S6a5NX6g6GNUta7Jd5-pKCTkzkc_NUOEBt1zQhsAJvRJuheeeKI7CXJG3k5SFAYSgFA0trBO-JHE6piohNxzhNt3VESczACvcUl3v7e6HXINydD2T3AtQRfz-HAynkNB0Q6JnCRbUoVjJZOI8HvTk9v6LUfjq_r3b7zvD9s-1XHRgOMdge4m-dj60HtJEuRN0Aful-0ZCw6ecQCZRy90UbMH_AwOo6kxPxx1-lf8ZfnpOYZvJEx40eVEb79ryBMQm_NpPmbKikOAUFiQyU0jUft2p68pNuTLwBrVMVQly-ZACHJk2yrtmMABCl17PEbOI70wZ21T-PwQdnxQzJWj7xmR4SJ-vu5GDnviLjo1_B0JJV9JsZSnZmmZaHk0I67Mhq6FISyX0fchCmOVSz27sd8XLYto8JDaxfCFwYYGu0owgvey03qXZKfw6XKP-eeeXEliWjadcun7AfqZmLyWGOoGCMWl29CgUqM7cPg0IH5NUgHETFCZw155fq1Z7cECbgAyxXszgvxPxiHGZ2fx-u8prUEWETzWvt1uFA3sG7N00JtKlSZfwusTajEfEvCQkjUYlnUbOz0fPDuiZyImHBqaKdZ-Nvy_IP19b-py-pZIJ5FJ6_--osgxghNQWfuv1FjhU5l8WLFbtqTo6NkRJY6hS1Bk7azS65TxXKFdx1qIbApFJxXhgcDcoY574lddUr8ZiEQriaB5P8N510ohEJTwYbAi2iH1X1-KVdgYN3PlshIqHyinAJM7BSaFYayIbCbudD0adhgpg7_U8Ar5xKsLpGCwRy47H0h72l7HWbVxJAby3l8Qj8idTYd6hC9YLMlrGcgv4Oiq_aroNv-T7vuB-nbo-uxGXy7JnPwqJZ6hzJc7J3kTgzzp0ftDepmBJdtUOmRwLKgJmsnZoPBl0bfq1pCrM5WVDYf2CJB1lQyGBPp7b5W7qOVIXrTtEqfEFA6dwciItfKQcmdoyY1gRuI7E6bZfMztGl3_pwsJz0weWjmIT6_vs-kfLdRBi4Zcod_km_ndwaiZbQWuEknhLjGj8OPd4cXvQc_sXStE3FD1qE-1yV__FF6eO9xx02p_uJmUdOyTq4lhXQueoY_9QqURz6ZhTMuxTnz26ZN3O4cHelQDEF1Zd9XPK8XDUiw8LZTisCT7KZQSJIwa5SaNroK-Ea24mGQpqv3Ib6hT9mrFfPoLWr_5ZruVBRrdZnQy6P60j29N__EhXrwjsh0CsgpB3gaGmYv-z-dvHBrmAUr8zQ../download",
+            "isStarred": false,
+            "modifiedAt": "2021-12-01T06:30:57.753951"
+        },
+        "890833148743": {
+            "name": "stl file 3.stl",
+            "isFolder": false,
+            "fileSize": 1789284,
+            "fileType": "stl",
+            "thumbnailLink": "https://patientfilethumbnail.s3.ap-southeast-2.amazonaws.com/stl%20file%2031638340253369.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211201T063054Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3598&X-Amz-Credential=AKIA4L27K4CWP2A2Y3FF%2F20211201%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Signature=ebdf03ead28cccbae4378eefd6ddb5df0756c6c161491d779558b63c0579dbd5",
+            "expirationTime": 1638343853369,
+            "thumbnailName": "stl file 31638340253369.png",
+            "downloadUrl": "https://dl.boxcloud.com/d/1/b1!ZrXcW-lnBd6yqC1z87CRa7mftys9cukNSHi9Rw_DujfcYNJSvYvMDDeDRdB-M5sJCmpcRA2LU_8k8f6V5XRIqcjnPeoihq8VZv6y2eN84_lpTHnUMLlacgnt9WsgOBsy_qSy__WypDxT7EkPi3U7bJE5vmV1j95NCtDB1sVIbutLgGoqIGvVq2hvDp-wWyDFmdAPK7EUDj-vJlde-MkpLEPdbJLt8pgWIDr4wM3ztjbyPHqqNeAFhH83Kwp1qYXfZyADCCo2Z0wFG0QiJRaZ3pd6PXm-fFuKCDXSCl5P2VdT3FpIh1GkTkSe8VifHj3XRuapkD8b0Z2AYByk3iXDWr4-NV-7SeD1GTCZQENY4jL9Q-En2LL5zPYmhGCpZMov3oihalmKx-bFTVy050OAzHy-TtkLB4QGJmrqecps11Gpg0-Lny4Et5vyHLSAIxEsecYyfymtCHuoFcgIjzH_Lms84PgPzEGdu_9yHx7LBWcRUSfT9V1xaNb5fRNyuUmUM2SYYzp9D7vZpPL3x15aiIL4aWF2mImXJPFvfy2YfhV9DTpWM70uEP332BIq0rSzpWRBEcxKDb9tux88sZYP_E8y63Z3py72cD8uSGlAmqRDosunFsbQ83EzxC0S_yc9kDg46i-J8mrpbfBbFktYsuUvoORbwY-RuDZsNVNhAcFkouFm_xyBYKzm2PuF-cFDWEMFYE02uf5D_WrdP90tvub27bza-1ZRNBMe4wOYroJY_M4OYqf2gBGMiVJYDfWgMa-9oQBTiEHG5f8doumNoAiOP9MGlFmN2z_1-S4RGawrlJdoYzaaDk2f6n8ziZYUGUt89ZM0qozVXR94Fz-v3ZwPx7f4cJA-XZhYbNbSD0DiX1FnEyybU4x83WTXrobNf0ytgXqEFt5kM9J28O1852b1IRPTgTadlVBqwqBabTV5zhHxOXR0P-kh7lXmHqnZkkxQ0VnA5S3bBYuOs_ZhicNeGKWy7sjhfjsBVZ5DDg0oAimyetTpIib0dS_33rM3ufuJoUc0id9Pfs18er9KPHTXzZHvGmKU7G68lViTq7J2E9xAHjlmg-d_KfBFqJu8IXQK0Y6jfdWK2aeezixQUpV39w1tYW9xzPFhkS9pAGLJT43JfTMP-Pcb0aC_Sja7uGlVth7IeHwWDdzk93smLbPOALf8gOyhh87GwbUaLBXA3FSVPG91CkrClrk3gW6LVlfRCLd_vXzumoyQY2ThhJCCWc2x6arFxcfOFx50NpfVYB7XS456qiUucExsTjhF6mhK7TRot03mMo9GKRoou0FiOK0DGINjyi-ct_o_5Ng./download",
+            "isStarred": false,
+            "modifiedAt": "2021-12-01T06:30:54.970248"
+        },
+        "890833318295": {
+            "name": "stl file 2.stl",
+            "isFolder": false,
+            "fileSize": 316084,
+            "fileType": "stl",
+            "thumbnailLink": "https://patientfilethumbnail.s3.ap-southeast-2.amazonaws.com/stl%20file%2021638340259605.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211201T063100Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIA4L27K4CWP2A2Y3FF%2F20211201%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Signature=aada8962d6d1e57957d96839bb8a2b0b901c91fd15cc0323a9308942d6f6fbf6",
+            "expirationTime": 1638343859605,
+            "thumbnailName": "stl file 21638340259605.png",
+            "downloadUrl": "https://dl.boxcloud.com/d/1/b1!WzkQfvM1037qzwKdWXZpYpJ9w6Lr1zsK57nPbdlw4-AvoAM9PkloADppeB9fmkV6NEwbXO6hXSAww6IdiC7DyX-3GHXfFH0N_N4uim3EI3uNE7OHfbJKKhKPvMElHuzlqAdW5IfrMA30JvYN9el-4LG_BVcJUpkOA6r0BIKJiZ_Q9TCChD0ohHo0O2-GrqShMMq_NrpkqpPau46YLSKS5fJFiAHcXzfv-SocQUyP8RulgBXbcu5qlo05lHuy2IHSFxo446cgtbUGa8ucw9kOBBlQqOs4odzLmEuGBjBBEVVVlb416eKVb1WVDWJIGXBvSB_-UsbvUAbDg1nrpfyGPghwbc69B_6eBJZ9ky6BTrTK37iS26Ff0IebR_0EJU3aSjcuQiw4s6MOIoBi2zJeXqYHnqQ5Rv-nYvSjMvRtLhIkuVeJrg2-bm923w-PtpbVcZQ0IiaEDFd-Vua9qjIjpdDk8IYAFZW-LN5QUefbCGpc3eXCG1ZF5OnQ2O5PQzhOwqZIYSp_xkPLadsy1vp6g3Y3M9U5jx42f1FlaqWfZB0kHPRRVUmorfm7G-WIPlQLPz2q-BWkWyubAhe3Yb4HMhbhhP3Z9enngcvIRBGsXfeeixKbBFRdEsJI40eS84OPe2z-wYbVv_1bzGIB6ZOIHecKNuqZyDqtKuPvuu42GoHAnm161OIxjy3URCrPqS_aL2lpNK0J2Wz3n-GplLjj_IV_TP8bj_SYm-Gu6SzELxwddUjNXK8l1OecKgbwcMPWcxmgfnaWavv6msquWofsjnHVf-UeHHwONV7-5e6FwWW495JJldkXPn3eMd0bxpCEntt-TioT6X9KnpLM5bClfB_GJSuj3uJ5vVJ9bFzDRDPKT23-beB3pOCpKoDJC_BbwNOlcH4dk8oZyhxWoG1G_jmQM4eiUlO1GcgkYZtDDtXH7S0e4__-trVBjZKArUUcjFzKN6899oVYlEITdkZu-yu65DAIzKzCYLXnkOzlQmaINtSskD-H4zwe8YmJMQ7anuJ_VWNteWtNM9zm55KNOOAC5PS1mpZFKMfigrboPPj9hsXwZU5mM8io88xPGnFtuNd-s6lzhbuDbSkBo2704jkk3l56ZiBVvL6kS1PDhj5fFutIGWiWia3OILK36spObP2G4fv5nYBLyFd8qlvhYDv3IqhQU_KqxyQH3INa1MfnRMkUIfIOVEpqkZj_3twEsxp5x7UTr_X5rNytfTiPcKTws2ld7x1Ad7GhJTPweBH4nA6I5cWqZ-IOZ4lUbAn7fPlt2zmjIg-yPyctfd8UnLHMQJ8gbFV5VpjtlxwhVGpnoQ61CXpQeRwcBwVczFS4mDC2kQk0ADj4/download",
+            "isStarred": false,
+            "modifiedAt": "2021-12-01T06:31:00.437987"
+        }
+    }
+};
+
+const parentId = uploadedFiles.parent_id;
+const contactId = uploadedFiles.contact_id;
+const fileBuffer = [
+    {
+        name:"stl file 1.stl",
+        extension:"stl",
+        scanType:"leftFeet"
+    },
+    {
+        name:"stl file 2.stl",
+        extension:"stl",
+        scanType:"rightFeet"
+    },
+    {
+        name:"stl file 3.stl",
+        extension:"stl",
+        scanType:"bothFeet"
+    }
+]
+
+// take the above input and print the API URLs
+//only fileId and scanTypes will change
+
+getScanTypesList();
+
+function getScanTypesList() {
+    const scanTypesList = [];
+    for (const fileId in uploadedFiles.box_metadata) {
+        // console.log("qq fileId",fileId);
+        const fileObj = uploadedFiles.box_metadata[fileId];
+        fileObj.id = fileId;
+        // console.log("qq fileObj",fileObj);
+        fileObj.scanType = getScanType(fileObj);
+        scanTypesList.push(fileObj);
+    }
+    console.log("qq result", scanTypesList);
+}
+
+function getScanType(fileObj){
+    for (let i = 0; i < fileBuffer.length; i++) {
+        const element = fileBuffer[i];
+        if(fileObj.fileType === "stl" && element.name === fileObj.name)
+            return element.scanType;
+    }
+    return null;
+}
