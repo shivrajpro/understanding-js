6a6d4293201bb9fa38ce8d567ff1a26a9dd5f58a
shivrajpro <shivrajhulbatte@gmail.com>
2022-02-02 23:40:40 +0530

    formatting

--- the_baker_problem.js
+++ the_baker_problem.js
@@ -12,32 +12,34 @@ simply 1 or 200).
  Ingredients that are not present in the objects, can be considered as 0.
  */
 
- // must return 2
- const a1 = cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+// must return 2
+const a1 = cakes(
+  { flour: 500, sugar: 200, eggs: 1 },
+  { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+);
 // must return 0
-const a2 = cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, 
-    {sugar: 500, flour: 2000, milk: 2000}
+const a2 = cakes(
+  { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 },
+  { sugar: 500, flour: 2000, milk: 2000 }
 );
 console.log(a1);
 console.log(a2);
 function cakes(recipe, available) {
-    // TODO: insert code
-    let numberOfCakes = 0;
-    if(Object.values(recipe) >= Object.values(available)){
-        for (const ing in recipe)
-            available[ing] -= recipe[ing];
-        while(hasMoreIngredients(available)){
-            for (const ing in recipe)
-                available[ing] -= recipe[ing];
-            numberOfCakes++;
-        }
+  // TODO: insert code
+  let numberOfCakes = 0;
+  if (Object.values(recipe) >= Object.values(available)) {
+    for (const ing in recipe) available[ing] -= recipe[ing];
+    while (hasMoreIngredients(available)) {
+      for (const ing in recipe) available[ing] -= recipe[ing];
+      numberOfCakes++;
     }
-    return numberOfCakes;
+  }
+  return numberOfCakes;
 }
 
 function hasMoreIngredients(ingredients) {
-    const amountAvailable = Object.values(ingredients);
-    for (let i = 0; i < amountAvailable.length; i++)
-        if(amountAvailable[i] <= 0) return false;
-    return true;
+  const amountAvailable = Object.values(ingredients);
+  for (let i = 0; i < amountAvailable.length; i++)
+    if (amountAvailable[i] <= 0) return false;
+  return true;
 }
