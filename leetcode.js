68656550f073b47ef952f866ff3b31b2d6c315b0
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2022-11-03 16:37:06 +0530

    single number and determine-if-two-events-have-conflict

--- leetcode.js
+++ leetcode.js
@@ -43,9 +43,59 @@ var containsNearbyDuplicate = function (nums, k) {
     return false;
 };
 
-console.log('>>', containsNearbyDuplicate([1, 2, 3, 1], 3));//true
-console.log('>>', containsNearbyDuplicate([1, 0, 1, 1], 1));//true
-console.log('>>', containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2));//false
-console.log('>>', containsNearbyDuplicate([99, 99], 2));//true
-console.log('>>', containsNearbyDuplicate([1, 2], 2));//false
-console.log('>>', containsNearbyDuplicate([1, 2, 1], 0));//false
+// console.log('>>', containsNearbyDuplicate([1, 2, 3, 1], 3));//true
+// console.log('>>', containsNearbyDuplicate([1, 0, 1, 1], 1));//true
+// console.log('>>', containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2));//false
+// console.log('>>', containsNearbyDuplicate([99, 99], 2));//true
+// console.log('>>', containsNearbyDuplicate([1, 2], 2));//false
+// console.log('>>', containsNearbyDuplicate([1, 2, 1], 0));//false
+
+// 3. https://leetcode.com/problems/single-number/
+const singleNumber =  function (nums) {
+    const tracker = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const e = nums[i];
+        if(!tracker[e])
+            tracker[e] = 1;
+        else
+            tracker[e] += 1;
+    }
+
+    // console.log('>> tracker',tracker);
+    for (const k in tracker) {
+        if(tracker[k] === 1)
+            return k;
+    }
+}
+
+// console.log(singleNumber([2,2,1]));
+// console.log(singleNumber([4,1,2,1,2]));
+// console.log(singleNumber([1]));
+
+// 4. https://leetcode.com/problems/determine-if-two-events-have-conflict
+const haveConflict = function (event1, event2) {
+    // Input: event1 = ["01:15","02:00"], event2 = ["02:00","03:00"]
+    // Output: true
+
+    // console.log("events",event1, event2);
+    //first store the hour in an arr1 and then compare
+
+
+    const [startA, endA] = event1;
+    const [startB, endB] = event2;
+
+    if (startA <= startB) {
+      if (endA >= startB) return true;
+    } else {
+      if (endB >= startA) return true;
+    }
+
+    return false;
+
+}
+
+console.log(haveConflict(["01:15","02:00"], ["02:00","03:00"]));
+console.log(haveConflict(["01:00","02:00"], ["01:20","03:00"]));
+console.log(haveConflict(["10:00","11:00"], ["14:00","15:00"]));
+console.log(haveConflict(["01:37","14:20"], ["05:06","06:17"]));
