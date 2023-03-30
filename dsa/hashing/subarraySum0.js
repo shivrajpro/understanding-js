e14c2aa8816ab0204f6c5f8e2c72e6b0d801eeb0
shivrajpro <shivrajhulbatte@gmail.com>
2023-03-30 22:10:49 +0530

    hashing: first repeating number

--- dsa/hashing/subarraySum0.js
+++ dsa/hashing/subarraySum0.js
@@ -1,6 +1,8 @@
 // https://www.scaler.com/academy/mentee-dashboard/class/59861/assignment/problems?navref=cl_tb_br
 
 function hasSubArr(A) {
+  // if a pf sum array includes 0 then there exists a subarray with sum 0
+  // if any number is repeating in pf sum then there exists a subarray with sum 0
   const pf = [A[0]];
 
   for (let i = 1; i < A.length; i++) pf[i] = pf[i - 1] + A[i];
