9fa760d891da62cc860dbcd25df892be9a899909
shivrajpro <shivrajhulbatte@gmail.com>
2023-11-02 23:53:08 +0530

    typescript primitive types part1

---
+++ FE-Kit/typescript/data-types/primitive-types.ts
@@ -0,0 +1,31 @@
+/*
+? Primitive types in TypeScript
+
+NOTES:
+- TypeScript has most of the JavaScript primitive types with the same names.
+*/
+
+//* string type
+let a: string = "Hello, world";
+
+//* number type
+let b: number = 10;
+
+//* boolean type
+let c: boolean = false;
+
+//* null type
+let d: null = null;
+
+//* undefined type
+let e: undefined = undefined;
+
+//* bigint type
+let f: bigint = BigInt(100);
+
+//* symbol type
+let g: symbol = Symbol("name")
+
+
+// ... and the rest of JS primitive types are also TS types.
+
