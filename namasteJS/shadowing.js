794174774023a6534779353e9043a434d74cd295
shivrajpro <shivrajhulbatte@gmail.com>
2023-11-10 01:18:45 +0530

    shadowing in js

---
+++ namasteJS/shadowing.js
@@ -0,0 +1,50 @@
+// var a = 100;
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);//b is not defined
+// console.log(c);
+
+// ==========================
+
+// let a = 100;
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(a); //identifier a already declared
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ==========================
+
+// let b = 100;
+// {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+// //   console.log(a);
+// //   console.log(b);
+// //   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);//c is not defined
+
+
+// ==========================
+// ILLEGAL SHADOWING
+// let a = 20;
+// {
+//     var a = 20;
+// }
