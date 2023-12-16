a5f8aec9ec992350b1387abe12dd8f0e587bf2ea
shivrajpro <shivrajhulbatte@gmail.com>
2023-12-17 00:33:39 +0530

    promise chain

--- namasteJS/promises/promises-1.js
+++ namasteJS/promises/promises-1.js
@@ -1,10 +1,13 @@
 const cart = ["apple", "banana"];
 
-// callback hell
+// callback hell & inversion of control (any function may fail)
 /*
+pyramid of doom
 api.createOrder(cart, (orderId)=>{
-    api.proceedPayment(orderId, ()=>{
-        api.showOrderSummary()
+    api.proceedPayment(orderId, (paymentInfop)=>{
+        api.showOrderSummary(function(paymentInfop){
+            api.updateWallet()
+        })
     })
 });
 */
@@ -12,3 +15,22 @@ api.createOrder(cart, (orderId)=>{
 const GITHUB_API = "https://api.github.com/users";
 const users = fetch(GITHUB_API);
 console.log(users);
+
+// promise chain
+/*
+createOrder(cart)
+    .then(function (orderId) {
+        return proceedToPayment(orderId)
+    })
+    .then(function (paymentInfo) {
+        return showOrderSummary(paymentInfo)
+    })
+    .then(function (paymentInfo) {
+        return updateWalletBalance(paymentInfo);
+    })
+
+createOrder(cart)
+    .then(orderId => proceedToPayment(orderId))
+    .then(paymentInfo => showOrderSummary(paymentInfo))
+    .then(paymentInfo => updateWalletBalance(paymentInfo))
+*/
