ef0cc671d6547e3344ee34df0263d1596414bc7e
shivrajpro <shivrajhulbatte@gmail.com>
2025-08-06 08:27:22 +0530

    arguments keyword in js

---
+++ index.js
@@ -0,0 +1,15 @@
+// const arr = [1, 2, 3];
+// const obj = { x: 1, y: 2, z: 3 };
+
+// console.log([...arr, ...obj]);
+function foo(a, b) {
+    console.log(arguments[1]);
+}
+
+foo(3, 1);
+
+// const foo1 = (a, b) => {
+//     console.log(arguments[1]); //error
+// }
+
+// foo1(2);
